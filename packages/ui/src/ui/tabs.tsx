3c6239265dd7e29eb9ec111d942e2d6f779a6e4a
Pasquale Vitiello <pasqualevitiello@gmail.com>
2025-10-20 15:14:32 +0200

    fix: issue with underline tab indicator (#448)

--- packages/ui/src/ui/tabs.tsx
+++ packages/ui/src/ui/tabs.tsx
@@ -46,7 +46,7 @@ function TabsList({
         className={cn(
           "absolute bottom-0 left-0 h-(--active-tab-height) w-(--active-tab-width) translate-x-(--active-tab-left) -translate-y-(--active-tab-bottom) transition-[width,translate] duration-200 ease-in-out",
           variant === "underline"
-            ? "z-10 bg-primary data-[orientation=horizontal]:-bottom-[calc(--spacing(1)+1px)] data-[orientation=horizontal]:h-0.5 data-[orientation=vertical]:-start-[calc(--spacing(1)+1px)] data-[orientation=vertical]:w-0.5"
+            ? "z-10 bg-primary data-[orientation=horizontal]:h-0.5 data-[orientation=horizontal]:translate-y-px data-[orientation=vertical]:w-0.5 data-[orientation=vertical]:-translate-x-px"
             : "-z-1 rounded-md bg-background shadow-sm dark:bg-accent"
         )}
       />
