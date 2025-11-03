13f4b69c73b7bf67b0cb29fefdd6e4b879515995
pasqualevitiello <pasqualevitiello@gmail.com>
2025-11-03 15:11:42 +0100

    chore: build registry

--- packages/ui/src/ui/textarea.tsx
+++ packages/ui/src/ui/textarea.tsx
@@ -1,4 +1,8 @@
+"use client"
+
 import * as React from "react"
+import { Field as FieldPrimitive } from "@base-ui-components/react/field"
+import { mergeProps } from "@base-ui-components/react/merge-props"
 
 import { cn } from "@workspace/ui/lib/utils"
 
@@ -25,16 +29,20 @@ function Textarea({
         ) || undefined
       }
     >
-      <textarea
-        data-slot="textarea"
-        className={cn(
-          "field-sizing-content min-h-17.5 w-full rounded-[inherit] px-[calc(--spacing(3)-1px)] py-[calc(--spacing(1.5)-1px)] outline-none max-sm:min-h-20.5",
-          size === "sm" &&
-            "min-h-16.5 px-[calc(--spacing(2.5)-1px)] py-[calc(--spacing(1)-1px)] max-sm:min-h-19.5",
-          size === "lg" &&
-            "min-h-18.5 py-[calc(--spacing(2)-1px)] max-sm:min-h-21.5"
+      <FieldPrimitive.Control
+        render={(controlProps) => (
+          <textarea
+            data-slot="textarea"
+            className={cn(
+              "field-sizing-content min-h-17.5 w-full rounded-[inherit] px-[calc(--spacing(3)-1px)] py-[calc(--spacing(1.5)-1px)] outline-none max-sm:min-h-20.5",
+              size === "sm" &&
+                "min-h-16.5 px-[calc(--spacing(2.5)-1px)] py-[calc(--spacing(1)-1px)] max-sm:min-h-19.5",
+              size === "lg" &&
+                "min-h-18.5 py-[calc(--spacing(2)-1px)] max-sm:min-h-21.5"
+            )}
+            {...mergeProps(controlProps, props)}
+          />
         )}
-        {...props}
       />
     </span>
   )
