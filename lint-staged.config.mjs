7b43bc22277d2c1ca7bb9a293fc8196b078ba150
Volnei Munhoz <volnei.munhoz@gmail.com>
2025-11-14 15:22:00 -0300

    Improve performance

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,5 @@
 export default {
-  "**/*.{js,ts,jsx,tsx,md,mdx,json}": (files) =>
-    `bunx biome check --write --no-errors-on-unmatched ${files.join(" ")}`,
+  "**/*.{js,ts,jsx,tsx,md,mdx,json}": () => [
+    "bunx biome check --write --no-errors-on-unmatched",
+  ],
 };
