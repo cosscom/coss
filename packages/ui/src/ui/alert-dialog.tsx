271d23b2f74af3ab0b9087bf2c4caae39740ed71
pasqualevitiello <pasqualevitiello@gmail.com>
2025-11-18 16:53:42 +0100

    chore: format

--- packages/ui/src/ui/alert-dialog.tsx
+++ packages/ui/src/ui/alert-dialog.tsx
@@ -64,8 +64,8 @@ function AlertDialogHeader({
 }: React.ComponentProps<"div">) {
   return (
     <div
-      data-slot="alert-dialog-header"
       className={cn("flex flex-col gap-1 text-center sm:text-left", className)}
+      data-slot="alert-dialog-header"
       {...props}
     />
   );
