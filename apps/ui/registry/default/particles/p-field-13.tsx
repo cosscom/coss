4dcf0c976de470578d5a286f30d66f3116b0f334
XIN <anxanxx@gmail.com>
2025-12-15 00:28:50 +0800

    refactor: restructure filed-14 to utilize FieldItem for checkbox group

--- apps/ui/registry/default/particles/p-field-13.tsx
+++ apps/ui/registry/default/particles/p-field-13.tsx
@@ -2,32 +2,34 @@
 
 import { Checkbox } from "@/registry/default/ui/checkbox";
 import { CheckboxGroup } from "@/registry/default/ui/checkbox-group";
-import { Field, FieldLabel } from "@/registry/default/ui/field";
+import { Field, FieldItem, FieldLabel } from "@/registry/default/ui/field";
 import { Fieldset, FieldsetLegend } from "@/registry/default/ui/fieldset";
 
 export default function Particle() {
   return (
-    <Fieldset className="gap-4">
-      <FieldsetLegend className="font-medium text-sm">
-        Frameworks
-      </FieldsetLegend>
-      <CheckboxGroup defaultValue={["react"]}>
-        <Field name="frameworks">
-          <FieldLabel>
-            <Checkbox value="react" /> React
-          </FieldLabel>
-        </Field>
-        <Field name="frameworks">
-          <FieldLabel>
-            <Checkbox value="vue" /> Vue
-          </FieldLabel>
-        </Field>
-        <Field name="frameworks">
-          <FieldLabel>
-            <Checkbox value="svelte" /> Svelte
-          </FieldLabel>
-        </Field>
-      </CheckboxGroup>
-    </Fieldset>
+    <Field name="frameworks">
+      <Fieldset
+        className="gap-4"
+        render={(props) => (
+          <CheckboxGroup {...props} defaultValue={["react"]} />
+        )}
+      >
+        <FieldsetLegend className="font-medium text-sm">
+          Frameworks
+        </FieldsetLegend>
+        <FieldItem>
+          <Checkbox value="react" />
+          <FieldLabel>React</FieldLabel>
+        </FieldItem>
+        <FieldItem>
+          <Checkbox value="vue" />
+          <FieldLabel>Vue</FieldLabel>
+        </FieldItem>
+        <FieldItem>
+          <Checkbox value="svelte" />
+          <FieldLabel>Svelte</FieldLabel>
+        </FieldItem>
+      </Fieldset>
+    </Field>
   );
 }
