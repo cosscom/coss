b40ae5ef7f9c337ccddc846c907c7f7e8084ac70
pasqualevitiello <pasqualevitiello@gmail.com>
2025-12-15 13:15:02 +0100

    chore: rewrite some tailwind classes and improve button

--- apps/ui/registry/default/ui/toggle.tsx
+++ apps/ui/registry/default/ui/toggle.tsx
@@ -21,7 +21,7 @@ const toggleVariants = cva(
       variant: {
         default: "border-transparent",
         outline:
-          "border-border bg-clip-padding shadow-xs not-disabled:not-active:not-data-pressed:before:shadow-[0_1px_--theme(--color-black/4%)] dark:bg-input/32 dark:hover:bg-input/64 dark:not-disabled:not-active:not-data-pressed:before:shadow-[0_-1px_--theme(--color-white/8%)] dark:not-disabled:not-data-pressed:before:shadow-[0_-1px_--theme(--color-white/4%)] [&:is(:disabled,:active,[data-pressed])]:shadow-none",
+          "border-border bg-clip-padding shadow-xs not-disabled:not-active:not-data-pressed:before:shadow-[0_1px_--theme(--color-black/4%)] dark:bg-input/32 dark:hover:bg-input/64 dark:not-disabled:not-active:not-data-pressed:before:shadow-[0_-1px_--theme(--color-white/8%)] dark:not-disabled:not-data-pressed:before:shadow-[0_-1px_--theme(--color-white/4%)] [:disabled,:active,[data-pressed]]:shadow-none",
       },
     },
   },
