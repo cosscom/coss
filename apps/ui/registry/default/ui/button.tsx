a67b08cd2bc374645817411f1f46282505567257
Pasquale Vitiello <pasqualevitiello@gmail.com>
2025-12-19 11:10:15 +0100

    fix: secondary button showing a border (#606)

--- apps/ui/registry/default/ui/button.tsx
+++ apps/ui/registry/default/ui/button.tsx
@@ -39,7 +39,7 @@ const buttonVariants = cva(
         outline:
           "border-border bg-background bg-clip-padding shadow-xs not-disabled:not-active:not-data-pressed:before:shadow-[0_1px_--theme(--color-black/4%)] dark:bg-input/32 dark:not-in-data-[slot=group]:bg-clip-border dark:not-disabled:before:shadow-[0_-1px_--theme(--color-white/4%)] dark:not-disabled:not-active:not-data-pressed:before:shadow-[0_-1px_--theme(--color-white/8%)] [:disabled,:active,[data-pressed]]:shadow-none [:hover,[data-pressed]]:bg-accent/50 dark:[:hover,[data-pressed]]:bg-input/64",
         secondary:
-          "border-secondary bg-secondary text-secondary-foreground hover:bg-secondary/90 data-pressed:bg-secondary/90",
+          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/90 [:active,[data-pressed]]:bg-secondary/80",
       },
     },
   },
