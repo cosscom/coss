d287b9a95e7a4b3a522324bd439babc4b8748756
pasqualevitiello <pasqualevitiello@gmail.com>
2026-01-14 12:48:53 +0100

    mc: remove label from p-input-17

--- apps/ui/registry/default/particles/p-input-17.tsx
+++ apps/ui/registry/default/particles/p-input-17.tsx
@@ -1,20 +1,16 @@
 import { useId } from "react";
 
 import { Input } from "@/registry/default/ui/input";
-import { Label } from "@/registry/default/ui/label";
 
 export default function Particle() {
   const id = useId();
   return (
-    <div className="flex flex-col gap-2">
-      <Label htmlFor={id}>Read-only input</Label>
-      <Input
-        className="read-only:bg-muted"
-        defaultValue="This is a read-only input"
-        id={id}
-        readOnly
-        type="text"
-      />
-    </div>
+    <Input
+      className="read-only:bg-muted"
+      defaultValue="This is a read-only input"
+      id={id}
+      readOnly
+      type="text"
+    />
   );
 }
