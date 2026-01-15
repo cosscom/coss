ee76e53c03360129dd74c893dce5421ee0dff6de
pasqualevitiello <pasqualevitiello@gmail.com>
2026-01-15 10:21:53 +0100

    mc: spacing improvements

--- apps/ui/registry/default/particles/p-textarea-15.tsx
+++ apps/ui/registry/default/particles/p-textarea-15.tsx
@@ -3,7 +3,7 @@ import { Textarea } from "@/registry/default/ui/textarea";
 
 export default function Particle() {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-2">
       <Textarea placeholder="Type your message here" />
       <Button className="self-start">Send</Button>
     </div>
