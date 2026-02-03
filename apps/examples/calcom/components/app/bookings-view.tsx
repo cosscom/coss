254afecf246979b3927bc7be02b5859b81329daa
pasqualevitiello <pasqualevitiello@gmail.com>
2026-02-03 18:30:10 +0100

    mc

--- apps/examples/calcom/components/app/bookings-view.tsx
+++ apps/examples/calcom/components/app/bookings-view.tsx
@@ -1,21 +1,26 @@
+"use client";
+
 import { CalendarIcon, LayoutListIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 import { TabbedNav } from "@/components/app/tabbed-nav";
 
-const viewTabs = [
-  {
-    icon: <LayoutListIcon className="size-4" />,
-    title: "List view",
-    url: "/booking/past",
-  },
-  {
-    icon: <CalendarIcon className="size-4" />,
-    title: "Calendar view",
-    url: "/booking/past?view=calendar",
-  },
-];
-
 function BookingsView() {
+  const pathname = usePathname();
+
+  const viewTabs = [
+    {
+      icon: <LayoutListIcon className="size-4" />,
+      title: "List view",
+      url: pathname,
+    },
+    {
+      icon: <CalendarIcon className="size-4" />,
+      title: "Calendar view",
+      url: `${pathname}?view=calendar`,
+    },
+  ];
+
   return <TabbedNav ariaLabel="View type" tabs={viewTabs} />;
 }
 
