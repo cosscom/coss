41a64bba490f9b99cc9f9eb033e8d9887cc443d3
pasqualevitiello <pasqualevitiello@gmail.com>
2026-02-11 13:07:40 +0100

    improve particles

--- apps/ui/registry/default/particles/p-calendar-1.tsx
+++ apps/ui/registry/default/particles/p-calendar-1.tsx
@@ -1,16 +1,11 @@
 "use client";
+
 import * as React from "react";
 
 import { Calendar } from "@/registry/default/ui/calendar";
 
 export default function Particle() {
   const [date, setDate] = React.useState<Date | undefined>(new Date());
-  return (
-    <Calendar
-      className="[--cell-size:--spacing(9)]"
-      mode="single"
-      onSelect={setDate}
-      selected={date}
-    />
-  );
+
+  return <Calendar mode="single" onSelect={setDate} selected={date} />;
 }
