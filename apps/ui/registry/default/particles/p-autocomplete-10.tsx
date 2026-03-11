528dc98c19f784b769be07dad1251163d855c3ad
pasqualevitiello <pasqualevitiello@gmail.com>
2026-03-11 18:08:17 +0100

    remove a few unneeded biome comments

--- apps/ui/registry/default/particles/p-autocomplete-10.tsx
+++ apps/ui/registry/default/particles/p-autocomplete-10.tsx
@@ -72,8 +72,7 @@ function groupTags(tags: Tag[]): TagGroup[] {
     if (!groups[tag.group]) {
       groups[tag.group] = [];
     }
-    // biome-ignore lint/style/noNonNullAssertion: will never be null
-    groups[tag.group]!.push(tag);
+    groups[tag.group]?.push(tag);
   }
 
   const order: Array<TagGroup["value"]> = ["Status", "Priority", "Team"];
